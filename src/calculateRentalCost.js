98876a03bfcd5a3f97fb58d814208f16d93146e4
Ayoub LEFHIM <alefhim@iliadcorp-q97pgk6h0y.homenet.telecomitalia.it>
2025-03-30 18:32:40 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,26 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentalCost = 40;
+  let cost = 0;
+
+  const LONG_TERM_DISCOUNT = 50;
+  const LONG_TERM_DAYS = 7;
+  const SHORT_TERM_DISCOUNT = 20;
+  const SHORT_TERM_DAYS = 3;
+
+  switch (true) {
+    case days < LONG_TERM_DAYS && days >= SHORT_TERM_DAYS:
+      cost = days * rentalCost - SHORT_TERM_DISCOUNT;
+      break;
+    case days >= LONG_TERM_DAYS:
+      cost = days * rentalCost - LONG_TERM_DISCOUNT;
+      break;
+    default:
+      cost = days * rentalCost;
+  }
+
+  return cost;
 }
 
 module.exports = calculateRentalCost;
